56b58bf14e523bc19bce9be9f7bdc7944e1bef41
ICHINOSE Shogo <shogo82148@gmail.com>
2026-05-06 22:52:49 +0900

    Add build step for dependencies in valkey build script

--- build-valkey.sh
+++ build-valkey.sh
@@ -97,6 +97,7 @@ echo "::group::build valkey"
     fi
 
     mkdir -p "$PREFIX"
+    make -C deps "-j$JOBS" V=1
     make install "-j$JOBS" PREFIX="$PREFIX" BUILD_TLS=yes OPENSSL_PREFIX="$PREFIX" V=1
 )
 echo "::endgroup::"
